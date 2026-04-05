301b4aa864ed2c34068132cfceba00517981c198
Muhammad Afzal <72257341+muhammadafzal-dev@users.noreply.github.com>
2026-04-05 15:45:32 +0500

    Create Navigation.js for setting up React Navigation

---
+++ Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './HomeScreen';
+import ProfileScreen from './ProfileScreen';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
